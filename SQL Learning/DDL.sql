f81c4cd0113e82b02227183c9ac97fabbcde4ce1
Bhavishya Gaur <bhavishyagaur6@gmail.com>
2026-09-09 16:41:43 +0530

    Update

--- SQL Learning/DDL.sql
+++ SQL Learning/DDL.sql
@@ -9,3 +9,21 @@ CREATE TABLE persons(
 	CONSTRAINT pk_persons PRIMARY KEY (id)
 
 )
+
+/* Add a new column called email to the persons table */
+
+ALTER TABLE persons
+ADD email VARCHAR(50) NOT NULL
+
+SELECT * FROM persons
+
+-- Remove the column phone from the persons table
+
+ALTER TABLE persons
+DROP COLUMN phone
+
+SELECT * FROM persons
+
+-- Delete the table persons from the database
+
+DROP TABLE persons
